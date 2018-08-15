fef003d7ca5a59b36d70130647452ddaf4567d5b
Tim Head <betatim@gmail.com>
2018-08-15 11:03:53 +0200

    Update deploy.py to include new hub name

--- deploy.py
+++ deploy.py
@@ -262,7 +262,7 @@ def main():
     argparser.add_argument(
         'chartname',
         help="Select which chart to deploy",
-        choices=['staginghub', 'earthhub', 'wshub', 'bootcamp-hub', 'monitoring']
+        choices=['staginghub', 'ea-hub', 'wshub', 'bootcamp-hub', 'monitoring']
     )
 
     args = argparser.parse_args()
