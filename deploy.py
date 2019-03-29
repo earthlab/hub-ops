7995c22fac2edadf941512371c1b48a65a00f5fe
Karen Cranston <karen.cranston@gmail.com>
2019-03-29 08:54:39 -0400

    using consistent hub naming

--- deploy.py
+++ deploy.py
@@ -267,7 +267,7 @@ def main():
     argparser.add_argument(
         'chartname',
         help="Select which chart to deploy",
-        choices=['staginghub', 'ea-hub', 'wshub', 'bootcamp-hub', 'nbgrader', 'monitoring']
+        choices=['staginghub', 'ea-hub', 'wshub', 'bootcamp-hub', 'nbgrader-hub', 'monitoring']
     )
 
     args = argparser.parse_args()
